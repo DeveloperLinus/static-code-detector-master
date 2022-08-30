0ef7c8ade6038a9f5d706542f017121009a4331c
huqh <huqh@itlong.com.cn>
2022-08-30 16:01:12 +0800

    git hook测试2

--- DetektTest/app/src/main/java/com/lovejjfg/detekttest/error/LargeClassTest.kt
+++ DetektTest/app/src/main/java/com/lovejjfg/detekttest/error/LargeClassTest.kt
@@ -4,54 +4,36 @@ import android.util.Log
 
 class LargeClassTest {
     fun test01() {
-        print()
     }
 
     fun test02() {
-        print()
     }
 
     fun test03() {
-        print()
     }
 
     fun test04() {
-        print()
     }
 
     fun test05() {
-        print()
     }
 
     fun test06() {
-        print()
     }
 
     fun test07() {
-        print()
     }
 
     fun test08() {
-        print()
     }
 
     fun test09() {
-        print()
     }
 
     fun test10() {
-        print()
     }
 
     fun test11() {
-        print()
-    }
-
-    fun test12() {
-        print()
-    }
-
-    fun test13() {
     }
 
     private fun print() {
