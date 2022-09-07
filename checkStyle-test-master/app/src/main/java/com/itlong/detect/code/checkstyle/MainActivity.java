344cf1707768ed95af60fc15bf3ce30d6ab79b1d
huqh <huqh@itlong.com.cn>
2022-09-07 16:57:23 +0800

    checkstyle

--- checkStyle-test-master/app/src/main/java/com/itlong/detect/code/checkstyle/MainActivity.java
+++ checkStyle-test-master/app/src/main/java/com/itlong/detect/code/checkstyle/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("CheckStyle", "test");
+        Log.d("CheckStyle", "test2");
     }
 }
